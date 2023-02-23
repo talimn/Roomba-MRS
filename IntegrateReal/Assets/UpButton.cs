16e9722d8cc7c44820cfc5d67b5f31f280170fe5
tayashlym <tayashlym@tamu.edu>
2023-02-22 22:20:32 -0600

    Updated Direction Buttons
    Direction buttons complete for Roomba 1, still need to incorporate override button and then replicate all code for Roomba 2.

--- IntegrateReal/Assets/UpButton.cs
+++ IntegrateReal/Assets/UpButton.cs
@@ -5,17 +5,18 @@ using UnityEngine.UI;
 
 public class UpButton : MonoBehaviour
 {
-    public Button m_UpButton, m_DownButton, m_LeftButton, m_RightButton, m_StopButton;
+    public Button m_UpButton, m_DownButton, m_LeftButton, m_RightButton, m_StopButton, m_OverrideButton;
     public GameObject rmba;
+    public GameObject toggler;
+    
+
     private float speed = 5.0f;
     private bool goUp = false;
     private bool goDown = false;
     private bool goLeft = false;
     private bool goRight = false;
     private bool goStop = false;
-    public string upInput;
-    private float horizontalInput;
-    private float forwardInput;
+    
 
     void Start()
     {
@@ -25,6 +26,7 @@ public class UpButton : MonoBehaviour
         m_LeftButton.onClick.AddListener(TaskLeft);
         m_RightButton.onClick.AddListener(TaskRight);
         m_StopButton.onClick.AddListener(TaskStop);
+        m_OverrideButton.onClick.AddListener(TaskOverride);
     }
 
     void TaskUp()
@@ -77,34 +79,47 @@ public class UpButton : MonoBehaviour
         goLeft = false;
         goRight = false;
     }
+
+    void TaskOverride()
+    {
+        Debug.Log("Override clicked!");
+
+    }
+
+    
+
     // Update is called once per frame
     void Update()
     {
-        if ((goUp) && (!goDown) && (!goRight) && (!goLeft) && (!goStop))
+        if ((goUp) && (!goDown) && (!goRight) && (!goLeft) && (!goStop) && (toggleChoice(toggler.GetComponent<Toggle>().isOn)))
         {
             transform.Translate(0.05f * Time.deltaTime * speed, 0f, 0f);
         }
         
-        else if((goDown) && (!goUp) && (!goRight) && (!goLeft) && (!goStop))
+        else if((goDown) && (!goUp) && (!goRight) && (!goLeft) && (!goStop) && (toggleChoice(toggler.GetComponent<Toggle>().isOn)))
         {
             transform.Translate(-0.05f * Time.deltaTime * speed, 0f, 0f);
         }
 
-        else if((!goDown) && (!goUp) && (!goRight) && (goLeft) && (!goStop))
+        else if((!goDown) && (!goUp) && (!goRight) && (goLeft) && (!goStop) && (toggleChoice(toggler.GetComponent<Toggle>().isOn)))
         {
             transform.Translate(0f, -0.5f * Time.deltaTime * speed, 0f);
         }
 
-        else if((!goDown) && (!goUp) && (goRight) && (!goLeft) && (!goStop))
+        else if((!goDown) && (!goUp) && (goRight) && (!goLeft) && (!goStop) && (toggleChoice(toggler.GetComponent<Toggle>().isOn)))
         {
             transform.Translate(0f, 0.5f * Time.deltaTime * speed, 0f);
         }
 
-        else if ((!goDown) && (!goUp) && (!goRight) && (!goLeft) && (goStop))
+        else if ((!goDown) && (!goUp) && (!goRight) && (!goLeft) && (goStop) && (toggleChoice(toggler.GetComponent<Toggle>().isOn)))
         {
             transform.Translate(0f * Time.deltaTime * speed, 0f * Time.deltaTime * speed, 0);
         }
     }
 
-    
+    public bool toggleChoice(bool togs)
+    {
+        return togs;
+    }
+
 }
